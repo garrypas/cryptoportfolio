f178594f9961e9d057ef99b4c41a58933025f3b3
Garry Passarella <mbauser@Garrys-MacBook-Air.local>
2018-08-18 14:24:19 +0100

    Remove react-navigation warning message

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './App';
 AppRegistry.registerComponent('cryptoportfolio', () => App);
+// For react-navigation warning
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
